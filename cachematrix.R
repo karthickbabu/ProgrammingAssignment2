ec1201b8abf67261cf2a5f0125f6c08d055d3135
Karthick Babu <karthickbabu@yahoo.com>
2022-01-09 12:14:29 -0800

    Week3 assignment for cached matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## Creates a "CacheMatrix" object that is capable of returning inverse matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedInverse <- NULL
+  
+  ## Returns original Matrix
+  get <- function() {
+    return(x)
+  }
+  
+  ## Resets object to new Matrix
+  set <- function(y) {
+    x <<- y
+    cachedInverse <<- NULL
+  }
+  
+  ## Inverse is calculated only once, cached for next request.
+  getInverse <- function() {
+    # If not cached, calculate and cache it for next time use
+    if(is.null(cachedInverse)) {
+      message("Solving and caching")
+      cachedInverse <<- solve(x)
+    } else {
+      message("Getting cached inverse")
+    }
+    
+    return(cachedInverse)
+  }
+  
+  list(get = get, set = set, 
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Returns the inverse matrix of "CacheMatrix" object. 
+## Computes if required otherwise returns cached solution.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Note that getInverse handles checking cache and calculating inverse if req
+  return(x$getInverse())
 }
+
+###
+## Testing
+# source("cacheMatrix.R")
+# a1 <- c(3, 2, 5)
+# a2 <- c(2, 3, 2)
+# a3 <- c(5, 2, 4)
+# A <- rbind(a1, a2, a3)
+# print(A)
+#
+# cm <- makeCacheMatrix(A)
+# print(cacheSolve(cm))
+# ### Expect to see "Solving and caching"
+# print(cacheSolve(cm))
+# ### Expect to see "Getting cached inverse"
+###
+
